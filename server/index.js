a1076e6c85da7c62251c61c7f2f9d368e1e30892
Kuba <kuba0506@gmail.com>
2017-06-28 13:38:38 +0200

    Upgrade to websocket server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 const path = require('path');
+const http = require('http');
 
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
+var port = process.env.PORT || 3000;
 
 var app = express();
-var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
+
+//event listener
+io.on('connection', (socket) => {
+    console.log(`--- New user connected ---`);
+
+    socket.on('disconnect', (socket) => {
+        console.log(`User was disconnected!`);
+    });
+});
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     return res.status(200).send(publicPath + '/index.html');
-// });
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+//listen invokes http.createServer, instead of express server we use http server
+server.listen(port, () => console.log(`Server is running on port ${port}`));
